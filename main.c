b78c56e77c10ad42dada19fee5c6ae00e2c28d4e
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-28 10:14:34 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,47 @@
+// Marvish Chandra
+
+#include <stdio.h>
+
+int main(color,trend){
+    if color == "light pink & white"{
+        printf("This color combination gives your nails a light pink color and shines white.");}
+    if trend == "light pink & white"{
+        printf("This trend represents neutrals, a look for every occasion while staying unnoticeable.");}
+    if color == "pink & green"{
+        printf("This color trend stands out, especially if you're trying to get people's attention.");}
+    if trend == "pink & green"{
+        printf("This color trend represents neons, these are great for the summer and to make a statement.");}
+    if color == "gray"{
+        printf("This color works for all skin tones and is a low-profile color to wear.");}
+    if trend == "gray"{
+        printf("It remains as one of the most popular nail tones that works for everyone.");}
+    if color == "red"{
+        printf("This color is great for multiple occasions, specifcally for a night out.");}
+    if trend == "red"{
+        printf("The color is timeless, but is more popular with people of lighter tones.");}
+    if color == "purple"{
+        printf("Darker colors such as this give an older look that are great for darker tones.");}
+    if trend == "purple"{
+        printf("Darker colors such as purple, chocolate brown, and dark blue show a '90s vibe.");}
+    if color == "silver or gold"{
+        printf("These metal colors are great for younger women that want to stand out.");}
+    if trend == "silver or gold"{
+        printf("This combination represents metallics that are popular all year.");}
+    if color == "base pink & layered white"{
+        printf("The color combination is great for layering your nails. They are subtle, but stand out with a small crowd.");}
+    if trend == "base pink & layered white"{
+        printf("The combination works for people of all skin tones and are great for any occasion.");}
+    if color == "dark green"{
+        printf("This color is better for women of darker skin tones since it is more subtle.");}
+    if trend = "dark green"{
+        printf("The color has been universally popular, specifically in 2020.");}
+    if color == "blue"{
+        printf("This is a classic color that looks well for most people.");}
+    if trend == "blue"{
+        printf("This gives a sporty look to your nails and gives patriotic vibes.");}
+    if color == "light yellow & green"{
+        printf("These light colors work great for younger women who want to give attention to their nails.");}
+    if trend == "light yellow & green"{
+        printf("These light colors are pastels that are bright and seek attention.");}
+    return 0;
+}
